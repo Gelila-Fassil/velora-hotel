28bddd5e816b40ba3598572d1a5f4e3fe8eb5c57
Gelila-Fassil <gelilafassil659@gmail.com>
2025-12-24 14:50:05 +0300

    changed the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,19 +22,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/faviconn.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/faviconn.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/faviconn.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/faviconn.png",
   },
 }
 
